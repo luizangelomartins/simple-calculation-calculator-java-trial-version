f07d0b9e44c6497fa9de2ef14f40a3f8d564483f
Luiz Ângelo Martins <Luiz ngelo Martins@LuizAngelo>
2020-11-27 21:06:24 -0200

    Commit feito para registrar o início do projeto:
    	- Início do projeto.
    	- Criação da classe que gera o menu.

---
+++ src/simplecalculator/Menu.java
@@ -0,0 +1,19 @@
+
+
+
+    // [ "CRIADOR - LUIZ ÂNGELO MARTINS" ]:
+
+
+
+// PACOTE DA CLASSE:
+package simplecalculator;
+
+
+// MÉTODO PRINCIPAL DA CLASSE:
+public class Menu {
+    
+}
+
+
+
+
---
+++ src/simplecalculator/SimpleCalculator.java
@@ -0,0 +1,26 @@
+
+
+
+    // [ "CRIADOR - LUIZ ÂNGELO MARTINS" ]:
+
+
+
+// PACOTE DA CLASSE:
+package simplecalculator;
+
+
+public class SimpleCalculator {
+
+    
+    // MÉTODO PRINCIPAL DA CLASSE:
+    public static void main(String[] args) {
+        
+        
+    }
+    
+    
+}
+
+
+
+
